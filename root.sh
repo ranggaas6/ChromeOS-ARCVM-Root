f88ff5a2cff2da23bebf2488bc34410e864d6b14
ranggaas6 <50361048+ranggaas6@users.noreply.github.com>
2024-08-01 23:02:18 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -12,7 +12,7 @@ RESET='\e[0m'
 BACKUP_PATH=/mnt/stateful_partition/arcvm_root
 KERNEL_PATH=/opt/google/vms/android
 
-KSU_VER='v0.8.1'
+KSU_VER='v0.7.6'
 KERNEL_VER='5.10.178'
 ARCH="`arch`"
 if [[ "$ARCH" =~ "aarch64" ]];then
